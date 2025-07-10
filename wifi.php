8385ff8c50518899f479decea659f1137bcf227e
TimoBekker <nikiforov93@mail.ru>
2025-07-10 17:07:02 +0400

    Update wifi.php

--- wifi.php
+++ wifi.php
@@ -4,7 +4,7 @@
 <meta charset="UTF-8" />
 <title>WiFi точки</title>
 <link rel="stylesheet" href="style.css" />
-<script src="https://api-maps.yandex.ru/2.1/?lang=ru_RU&apikey=3fe09804-b154-4a4e-b76c-4cc21123eb54" type="text/javascript"></script>
+<script src="https://api-maps.yandex.ru/2.1/?lang=ru_RU&apikey=xxx" type="text/javascript"></script>
 </head>
 <body>
 <h1>Публичные точки доступа к сети Интернет</h1>
